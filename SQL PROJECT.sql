848ba1387fc80ecd5d6f4201d8347e90fc2f1ec6
Riya Goyal <131674407+riyagoyal26@users.noreply.github.com>
2024-02-20 16:29:58 +0530

    Update SQL PROJECT.sql

--- SQL PROJECT.sql
+++ SQL PROJECT.sql
@@ -137,9 +137,4 @@ WITH Customter_with_country AS (
 SELECT * FROM Customter_with_country WHERE RowNo <= 1
 
 
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
-WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
+
